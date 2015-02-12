0c0029de34d8653fa78e2c246d3fb4b1672a30bf
drakontia <mhden@drakontia.com>
2015-02-12 17:55:16 +0900

    Correct db settings

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -7,9 +7,19 @@ return array(
 	'production' => array(
         'type' => 'mysqli',
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=advanced_combat_tracker',
+            'hostname'   => 'localhost',
+            'port'       => '3306',
+			'database'   => 'advanced_combat_tracker',
 			'username'   => 'user_act',
 			'password'   => 'ba7yd5lv',
+            'persistent' => false,
+            'compress'   => false,
 		),
+        'identifier' => '`',
+        'table_prefix' => '',
+        'charset' => 'utf8',
+        'enable_cache' => true,
+        'profiling' => false,
+        'readonly' => false,
 	),
 );
