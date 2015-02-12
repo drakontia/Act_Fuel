f970334cbd13a46ca23f3e5cd0fd9b622ee2b152
drakontia <mhden@drakontia.com>
2015-02-12 17:57:05 +0900

    Add migration result

---
+++ fuel/app/config/development/migrations.php
@@ -0,0 +1,39 @@
+<?php
+return array(
+  'version' => 
+  array(
+    'app' => 
+    array(
+      'default' => 
+      array(
+        0 => '001_create_attacktype_tables',
+        1 => '002_create_combatant_tables',
+        2 => '003_create_current_tables',
+        3 => '004_create_damagetype_tables',
+        4 => '005_create_encounter_tables',
+        5 => '006_create_swing_tables',
+      ),
+    ),
+    'module' => 
+    array(
+    ),
+    'package' => 
+    array(
+      'auth' => 
+      array(
+        0 => '001_auth_create_usertables',
+        1 => '002_auth_create_grouptables',
+        2 => '003_auth_create_roletables',
+        3 => '004_auth_create_permissiontables',
+        4 => '005_auth_create_authdefaults',
+        5 => '006_auth_add_authactions',
+        6 => '007_auth_add_permissionsfilter',
+        7 => '008_auth_create_providers',
+        8 => '009_auth_create_oauth2tables',
+        9 => '010_auth_fix_jointables',
+      ),
+    ),
+  ),
+  'folder' => 'migrations/',
+  'table' => 'migration',
+);
