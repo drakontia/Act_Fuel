e4aef749579ffd52830467279a6f9b74ff5834f5
drakontia <mhden@drakontia.com>
2015-02-19 12:04:09 +0900

    Decrease column of swing

--- fuel/app/views/swing/view.php
+++ fuel/app/views/swing/view.php
@@ -1,37 +1,31 @@
-<h2>Listing <span class='muted'>Swings</span></h2>
+<h2>Listing <span class='muted'>Swings of <?php echo $name ?></span></h2>
 <br>
 <?php if ($swing): ?>
-<table class="table table-striped">
+<table class="table table-bordered">
 	<thead>
 		<tr>
 			<th>Stime</th>
-			<th>Attacker</th>
 			<th>Attacktype</th>
 			<th>Damagetype</th>
 			<th>Victim</th>
 			<th>Damage</th>
-			<th>Damagestring</th>
 			<th>Critical</th>
 			<th>Special</th>
 			<th>Dmgadjust</th>
-			<th>Dmgreduced</th>
 			<th>Overheal</th>
 		</tr>
 	</thead>
 	<tbody>
 <?php foreach ($swing as $item): ?>
         <tr>
-			<td><?php echo $item->stime; ?></td>
-			<td><?php echo $item->attacker; ?></td>
+			<td><?php echo strftime('%c', strtotime($item->stime)); ?></td>
 			<td><?php echo $item->attacktype; ?></td>
 			<td><?php echo $item->damagetype; ?></td>
 			<td><?php echo $item->victim; ?></td>
 			<td><?php echo $item->damage; ?></td>
-			<td><?php echo $item->damagestring; ?></td>
 			<td><?php echo $item->critical; ?></td>
 			<td><?php echo $item->special; ?></td>
 			<td><?php echo $item->dmgadjust; ?></td>
-			<td><?php echo $item->dmgreduced; ?></td>
 			<td><?php echo $item->overheal; ?></td>
 		</tr>
 <?php endforeach; ?>
