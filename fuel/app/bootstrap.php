f2610889244d6b788bfa010427cbe35a10c50b34
drakontia <mhden@drakontia.com>
2015-02-19 14:05:36 +0900

    Internationalization for Web Applicaiton

--- fuel/app/bootstrap.php
+++ fuel/app/bootstrap.php
@@ -23,3 +23,35 @@ Fuel::$env = (isset($_SERVER['FUEL_ENV']) ? $_SERVER['FUEL_ENV'] : Fuel::DEVELOP
 
 // Initialize the framework with the config file.
 Fuel::init('config.php');
+
+//Internationalization of Web Application
+$languages = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+if (isset($languages[0]))
+{
+    $langfile = File::read_dir(APPPATH.'lang/'.$language[0], 0, array(
+        '!^\.',
+        '\.php$' => 'file',
+        '\.yaml$' => 'file',
+        '!^_',
+    ));
+
+    if (isset($langfile))
+    {
+        Config::set('language', $language[0]);
+    }
+    else
+    {
+        $lang2 = Str::sub($language[0], 0, 2);
+        $langfile2 = File::read_dir(APPPATH.'lang/'.$lang2, 0, array(
+            '!^\.',
+            '\.php$' => 'file',
+            '\.yaml$' => 'file',
+            '!^_',
+        ));
+        if (isset($langfile2))
+        {
+            Config::set('language', $lang2);
+        }
+        // else { use default language }
+    }
+}
