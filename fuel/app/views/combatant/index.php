8c0d76d218544f4cd12af040fedb19ebca8744c2
drakontia <mhden@drakontia.com>
2015-03-05 18:21:16 +0900

    improve compare view

--- fuel/app/views/combatant/index.php
+++ fuel/app/views/combatant/index.php
@@ -11,32 +11,32 @@
 <div class="form-group">
     <?php echo Form::label('実施場所A', 'encountera', array('class' => 'col-sm-2 control-label')); ?>
     <div class="col-sm-10">
-        <?php echo Form::select('EncounterA', 'none', $elist); ?>
+        <?php echo Form::select('encountera', $elist); ?>
     </div>
 </div>
 <div class="form-group">
-    <?php echo Form::label('実施場所A', 'encountera', array('class' => 'col-sm-2 control-label')); ?>
+    <?php echo Form::label('実施者A', 'titlea', array('class' => 'col-sm-2 control-label')); ?>
     <div class="col-sm-10">
-        <?php echo Form::select('EncounterA', 'none', $elist); ?>
+        <?php echo Form::select('titlea', array('none' => 'none')); ?>
     </div>
 </div>
 <div class="form-group">
-    <?php echo Form::label('実施場所A', 'encountera', array('class' => 'col-sm-2 control-label')); ?>
+    <?php echo Form::label('実施場所B', 'encounterb', array('class' => 'col-sm-2 control-label')); ?>
     <div class="col-sm-10">
-        <?php echo Form::select('EncounterA', 'none', $elist); ?>
+        <?php echo Form::select('encounterb', $elist); ?>
     </div>
 </div>
 <div class="form-group">
-    <?php echo Form::label('実施場所A', 'encountera', array('class' => 'col-sm-2 control-label')); ?>
+    <?php echo Form::label('実施者B', 'titleb', array('class' => 'col-sm-2 control-label')); ?>
     <div class="col-sm-10">
-        <?php echo Form::select('EncounterA', 'none', $elist); ?>
+        <?php echo Form::select('titleb', array('none' => 'none')); ?>
     </div>
 </div>
 <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
-        <?php echo Form::button('name', 'value', array('style' => 'border: 2px;')); ?>
+        <?php echo Form::button('compare', '比較', array('class' => 'button btn-primary')); ?>
     </div>
-</div>f
+</div>
 <?php echo Form::close(); ?>
 <?php else: ?>
 <p>No Combatants.</p>
