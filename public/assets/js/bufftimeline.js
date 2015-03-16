41b1c1d651e1d60c28ddc59ecc143b902001e56e
drakontia <mhden@drakontia.com>
2015-03-16 19:16:20 +0900

    addColumns and draw?

--- public/assets/js/bufftimeline.js
+++ public/assets/js/bufftimeline.js
@@ -19,6 +19,7 @@ $(document).ready(function(){
       dataTable.addColumn({ type: 'string', id: 'Victim' });
       dataTable.addColumn({ type: 'date', id: 'Start' });
       dataTable.addColumn({ type: 'date', id: 'End' });
+      chart.draw(dataTable, options);
     },
 
     'drawChart': function() {
