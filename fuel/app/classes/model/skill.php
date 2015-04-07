f80a366719e27cc748d78d45853281de4d89581b
drakontia <mhden@drakontia.com>
2015-04-07 16:35:14 +0900

    Must having 3 argument for validation

--- fuel/app/classes/model/skill.php
+++ fuel/app/classes/model/skill.php
@@ -18,8 +18,8 @@ class Model_Skill extends Model
     $val = Validation::forge($factory);
     $val->add_field('name', 'Name', 'required|max_length[64]');
     $val->add_field('swingtype', 'Swingtype', 'required');
-    $val->add_field('duration', 'Duration');
-    $val->add_field('recst', 'Recast');
+    $val->add_field('duration', 'Duration', 'required');
+    $val->add_field('recst', 'Recast', 'required');
 
     return $val;
   }
